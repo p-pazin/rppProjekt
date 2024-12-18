d9b3e24900d8983bd73d76a6ca83fca8a9165818
Avinkovic22 <avinkovic22@student.foi.hr>
2024-12-18 18:30:26 +0100

    Dodane rute, servisi i repozitorij za ugovore

---
+++ Software/Backend/CarchiveAPI/CarchiveAPI/Services/ContractServices.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CarchiveAPI.Dto;
+using CarchiveAPI.Models;
+using CarchiveAPI.Repositories;
+
+namespace CarchiveAPI.Services
+{
+    public class ContractServices
+    {
+        private readonly ContractRepository _contractRepository;
+        private readonly CompanyServices _companyServices;
+        private readonly IMapper _mapper;
+
+        public ContractServices(ContractRepository contractRepository, CompanyServices companyServices, IMapper mapper)
+        {
+            this._contractRepository = contractRepository;
+            this._companyServices = companyServices;
+            this._mapper = mapper;
+        }
+
+        public ICollection<ContractDto> GetContracts(string email)
+        {
+            int companyId = _companyServices.GetCompanyId(email);
+            var contracts = _contractRepository.GetContracts(companyId);
+            return _mapper.Map<List<ContractDto>>(contracts);
+        }
+
+        public bool CheckIfContractExists(int contractId)
+        {
+            return _contractRepository.ContractExists(contractId);
+        }
+
+        public Contract GetContract(int contractId, string email)
+        {
+            int companyId = _companyServices.GetCompanyId(email);
+            var contract = _contractRepository.GetContract(contractId, companyId);
+            return _mapper.Map<Contract>(contract);
+        }
+        public ContractDto MapContract(Contract contract)
+        {
+            return _mapper.Map<ContractDto>(contract);
+        }
+        public bool DeleteContract(Contract contractToDelete)
+        {
+            if (contractToDelete == null)
+            {
+                return false;
+            }
+            return _contractRepository.DeleteContract(contractToDelete);
+        }
+    }
+}
