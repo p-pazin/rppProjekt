4961ea4247a1d4786687b6122fb06b0469ba7ce5
ppazin <ppazin22@student.foi.hr>
2025-01-31 16:38:01 +0100

    Dizajniran GUI za katalog vozila i dodavanje #7 #5

---
+++ Software/Frontend/PresentationLayer/PresentationLayer/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using PresentationLayer.UserControls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PresentationLayer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigationMenu.SelectionChanged += NavigationMenu_SelectionChanged;
+
+            NavigationMenu.SelectedIndex = 0;
+            ToggleDrawerButton.Content = "✕";
+
+            this.SizeChanged += MainWindow_SizeChanged;
+
+            Drawer.IsVisibleChanged += Drawer_IsVisibleChanged;
+        }
+
+        private void NavigationMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NavigationMenu.SelectedItem is ListBoxItem selectedItem)
+            {
+                switch (selectedItem.Content.ToString())
+                {
+                    case "Nadzorna ploča":
+                        MainContentFrame.Content = null;
+                        break;
+                    case "Katalog vozila":
+                        MainContentFrame.Navigate(new UCVehicleCatalog());
+                        break;
+                    default:
+                        MainContentFrame.Content = null;
+                        break;
+                }
+            }
+        }
+
+        private void ToggleDrawerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Drawer.Visibility == Visibility.Visible)
+            {
+                Drawer.Visibility = Visibility.Collapsed;
+                ToggleDrawerButton.Content = "☰";
+                ToggleDrawerButton.Margin = new Thickness(-210, 10, 0, 0);
+            }
+            else
+            {
+                Drawer.Visibility = Visibility.Visible;
+                ToggleDrawerButton.Content = "✕";
+                ToggleDrawerButton.Margin = new Thickness(10, 10, 0, 0);
+            }
+
+            AdjustUserControlMargin();
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            AdjustUserControlMargin();
+        }
+
+        private void Drawer_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            AdjustUserControlMargin();
+        }
+
+        public void AdjustUserControlMargin()
+        {
+            if (MainContentFrame.Content is UserControl uc)
+            {
+                if (Drawer.Visibility == Visibility.Visible)
+                {
+                    uc.Margin = new Thickness(0);
+                }
+                else
+                {
+                    uc.Margin = new Thickness(-220, 0, 0, 0);
+                }
+            }
+        }
+
+        public void LoadUC(UserControl uc)
+        {
+            MainContentFrame.Content = uc;
+        }
+    }
+}
---
+++ Software/Frontend/PresentationLayer/PresentationLayer/UserControls/UCVehicleCatalog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UCVehicleCatalog.xaml
+    /// </summary>
+    public partial class UCVehicleCatalog : UserControl
+    {
+        public UCVehicleCatalog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow is MainWindow mw)
+            {
+                mw.LoadUC(new UCAddVehicle());
+                mw.AdjustUserControlMargin();
+            }
+        }
+    }
+}
