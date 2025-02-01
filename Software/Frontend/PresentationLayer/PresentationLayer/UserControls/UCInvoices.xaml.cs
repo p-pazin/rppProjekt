8799310d0ff14284a8e15a69ff94f27d9230bbdc
Avinkovic22 <avinkovic22@student.foi.hr>
2025-02-01 17:22:35 +0100

    Napravljen prikaz računa

--- Software/Frontend/PresentationLayer/PresentationLayer/UserControls/UCInvoices.xaml.cs
+++ Software/Frontend/PresentationLayer/PresentationLayer/UserControls/UCInvoices.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CarchiveAPI.Dto;
+using ServiceLayer.Services;
 
 namespace PresentationLayer.UserControls
 {
@@ -20,9 +22,51 @@ namespace PresentationLayer.UserControls
     /// </summary>
     public partial class UCInvoices : UserControl
     {
+        private readonly InvoiceService _invoiceService;
+
         public UCInvoices()
         {
             InitializeComponent();
+            selectedWarning.Visibility = Visibility.Hidden;
+            _invoiceService = new InvoiceService();
+        }
+
+        private void btnAddInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrintInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedInvoice = dgvInvoices.SelectedItem as InvoiceDto;
+
+            if (selectedInvoice != null)
+            {
+                
+            }
+            else
+            {
+                selectedWarning.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadInvoiceData();
+        }
+
+        public async void LoadInvoiceData()
+        {
+            try
+            {
+                List<InvoiceDto> invoices = await _invoiceService.GetInvoicesAsync();
+                dgvInvoices.ItemsSource = invoices;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Greška pri dohvaćanju podataka: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
         }
     }
 }
