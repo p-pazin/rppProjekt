0555413ffba94d41eb2353f41f0004ea7284ba71
ppazin <ppazin22@student.foi.hr>
2025-02-01 17:41:10 +0100

    Implementirana logika za dodavanje, brisanje i uređivanje ponuda. #22 #21 #20

--- Software/Frontend/PresentationLayer/ServiceLayer/Services/OfferServices.cs
+++ Software/Frontend/PresentationLayer/ServiceLayer/Services/OfferServices.cs
@@ -1,9 +1,11 @@
-﻿using ServiceLayer.Network.Dto;
+﻿using CarchiveAPI.Dto;
+using ServiceLayer.Network.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -39,5 +41,95 @@ namespace ServiceLayer.Services
             return JsonSerializer.Deserialize<List<OfferDto>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         }
 
+        public async Task<List<VehicleDto>> GetVehiclesForOffer(int offerId)
+        {
+            string token = _tokenManager.GetToken();
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("User is not logged in.");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"Vehicle/offer/{offerId}");
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to fetch offer data.");
+            string json = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<List<VehicleDto>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<ContactDto> GetContactForOffer(int offerId)
+        {
+            string token = _tokenManager.GetToken();
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("User is not logged in.");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"Contact/contacts/{offerId}");
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to fetch offer data.");
+
+            string json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<ContactDto>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task PostOffer(OfferDto offer, List<VehicleDto> vehicles, ContactDto contact)
+        {
+            string token = _tokenManager.GetToken();
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("User is not logged in.");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            string json = JsonSerializer.Serialize(offer);
+
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            List<int> vehicleIds = vehicles.Select(v => v.Id).ToList();
+
+            string requestUri = $"Offer?contactId={contact.Id}&{string.Join("&", vehicleIds.Select(id => $"vehiclesId={id}"))}";
+
+            HttpResponseMessage response = await _httpClient.PostAsync(requestUri, content);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to add offer data.");
+        }
+
+
+        public async Task PutOffer(OfferDto offer, List<VehicleDto> vehicles, ContactDto contact)
+        {
+            string token = _tokenManager.GetToken();
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("User is not logged in.");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            string json = JsonSerializer.Serialize(offer);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            List<int> vehicleIds = vehicles.Select(v => v.Id).ToList();
+
+            string queryString = $"?contactId={contact.Id}&vehiclesId={string.Join("&vehiclesId=", vehicleIds)}";
+
+            HttpResponseMessage response = await _httpClient.PutAsync($"Offer{queryString}", content);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to update offer data.");
+        }
+
+        public async void DeleteOffer(int offerId)
+        {
+            string token = _tokenManager.GetToken();
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("User is not logged in.");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"Offer/{offerId}");
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Failed to delete offer data.");
+        }
     }
 }
